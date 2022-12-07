2fdf479cfa1e4b4c36dae2868435ecaf8d5dc3a5
HeleneGreat <helene.dev@yahoo.com>
2022-12-07 19:26:42 +0100

    categories as twig global variables #48

---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use App\Repository\CategoryRepository;
+use Twig\Environment;
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+
+    private $twig;
+    private $categoryRepository;
+
+    public function __construct(Environment $twig, CategoryRepository $categoryRepository)
+    {
+        $this->twig = $twig;
+        $this->categoryRepository = $categoryRepository;
+        
+    }
+
+    public function onControllerEvent(ControllerEvent $event): void
+    {
+        $this->twig->addGlobal('categories', $this->categoryRepository->findAll());
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ControllerEvent::class => 'onControllerEvent',
+        ];
+    }
+}
