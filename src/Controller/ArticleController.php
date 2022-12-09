9d25f360310834a41fcfbb0b3ddb42f25bf174ae
Alan Dauphin <alandauphin@hotmail.fr>
2022-12-09 22:16:53 +0100

    article controller pour ajout d'image dans le formulaire partie 2 - #64

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Article;
 use App\Entity\Category;
@@ -43,7 +45,7 @@ class ArticleController extends AbstractController
 
     // Form to add a new article
     #[Route('/article/add', name: 'article_add', requirements:['add' => 'a-zA-Z'])]
-    public function createArticle(ManagerRegistry $doctrine, Request $request):Response
+    public function createArticle(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger):Response
     {
         // User must be registered to access this page
         if($this->getUser()){
@@ -53,6 +55,30 @@ class ArticleController extends AbstractController
             $form->handleRequest($request);
 
             if($form->isSubmitted() && $form->isValid()){
+                $imageFile = $form->get('image')->getData();
+
+            // this condition is needed because the 'image' field is not required
+            // so the image file must be processed only when a file is uploaded
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $imageFile->move(
+                        $this->getParameter('article_image'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }  
+
+                // updates the 'imageFilename' property to store the PDF file name
+                // instead of its contents
+                $article->setImage($newFilename);
+            }
                 $article = $form->getData();
                 $entityManager = $doctrine->getManager();
                 $entityManager->persist($article);
