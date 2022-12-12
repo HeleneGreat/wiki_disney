1bf55a31438c714c6ec89993c38aff029377266e
Emeric <emeric.pwen@gmail.com>
2022-12-12 16:07:57 +0100

    #55 & #7 Page D'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        //Récupère et mélange tout les articles
+        $getArticles = $doctrine->getRepository(Article::class)->findAll();
+        shuffle($getArticles);
+
+        //Récupère et mélange toutes les catégories
+        $getCategory = $doctrine->getRepository(Category::class)->findAll();
+        shuffle($getCategory);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'all_articles' => $getArticles,
+            'all_cats' => $getCategory,
         ]);
     }
 }
